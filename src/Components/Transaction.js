8b5e9c8f98669d8eb5a7693a24eda0ee86f0b7c9
12C0DE <rubenhnt@gmail.com>
2020-06-22 15:10:11 -0500

    toggle income or expense on transaction

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,12 +1,32 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../Context/GlobalState';
 
+import Firebase from '../Firebase/Firebase';
+
 export const Transaction = ({ transaction }) => {
 	const { toggleCatAdded } = useContext(GlobalContext);
-	const sign = transaction.amount > 0 ? '+' : '-';
+	const isPositive = transaction.amount > 0 ? true : false;
+	const sign = isPositive ? '+' : '-';
+	const switchInEx = isPositive > 0 ? 'Switch to Expense' : 'Switch to Income';
+
+	function toggleIncomeExpense(docID, amt) {
+		Firebase.firestore().collection('transactions').doc(docID).update({
+			amount: amt * -1
+		});
+	}
 
 	return (
 		<li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+			<button
+				// className="switch-btn"
+				className={isPositive ? 'switch-btn switch-neg' : 'switch-btn switch-pos'}
+				onClick={() => {
+					toggleCatAdded(false);
+					toggleIncomeExpense(transaction.id, transaction.amount);
+				}}
+			>
+				{switchInEx}
+			</button>
 			<span className="transDate">{transaction.transDate}</span>
 			<span className="notes">{transaction.text}</span>
 			<span className="transAmount">
@@ -14,14 +34,6 @@ export const Transaction = ({ transaction }) => {
 				$
 				{Math.abs(transaction.amount)}
 			</span>
-			<button
-				className="delete-btn"
-				onClick={() => {
-					toggleCatAdded(false);
-				}}
-			>
-				x
-			</button>
 		</li>
 	);
 };
