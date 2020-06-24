260c39d5ac7777444727e66cfbc5f3d791e0ffe6
12C0DE <rubenhnt@gmail.com>
2020-06-24 15:13:11 -0500

    adding icon

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -5,9 +5,9 @@ import Firebase from '../Firebase/Firebase';
 
 export const Transaction = ({ transaction }) => {
 	const { toggleCatAdded } = useContext(GlobalContext);
-	const isPositive = transaction.amount > 0 ? true : false;
-	const sign = isPositive ? '+' : '-';
-	const switchInEx = isPositive > 0 ? 'Switch to Expense' : 'Switch to Income';
+	const amountPositive = transaction.amount > 0 ? true : false;
+	const sign = amountPositive ? '+' : '-';
+	const switchIncExp = amountPositive > 0 ? 'Switch to Expense' : 'Switch to Income';
 
 	function toggleIncomeExpense(docID, amt) {
 		Firebase.firestore().collection('transactions').doc(docID).update({
@@ -16,16 +16,15 @@ export const Transaction = ({ transaction }) => {
 	}
 
 	return (
-		<li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+		<li className={amountPositive < 0 ? 'minus' : 'plus'}>
 			<button
-				// className="switch-btn"
-				className={isPositive ? 'switch-btn switch-neg' : 'switch-btn switch-pos'}
+				className={amountPositive ? 'switch-btn switch-neg' : 'switch-btn switch-pos'}
 				onClick={() => {
 					toggleCatAdded(false);
 					toggleIncomeExpense(transaction.id, transaction.amount);
 				}}
 			>
-				{switchInEx}
+				{switchIncExp}
 			</button>
 			<span className="transDate">{transaction.transDate}</span>
 			<span className="notes">{transaction.text}</span>
